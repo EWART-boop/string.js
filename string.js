b3841935bd3b29b63e6f617744c298682199fc0e
EWART-boop <johnewartdelacruz@gmail.com>
2024-09-15 16:15:34 +0800

    strings.js.

---
+++ string.js
@@ -0,0 +1,20 @@
+let favActorfirstName = 'Tom';
+let favactorLastname = 'Cruise';
+let fullname = favActorfirstName + ' '  + favactorLastname;
+
+
+let upperCase = 'tom cruise';
+let lowerCase = 'DOG';
+
+
+let UpperCase = 'TOM CRUISE ';
+let message = ('My favorie actor is' + ' ' + UpperCase)
+message += 'His best show is Mission Imposible';
+
+
+console.log("favActorfirstName: ", favActorfirstName);
+console.log("favactorLastname: ", favactorLastname);
+console.log("fullname: ", fullname);
+console.log("upperCase: ", upperCase);
+console.log("lowerCase: ", lowerCase);
+console.log("message: ", message);
